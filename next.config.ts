13e8f6b9e3a5868c7b2e4b8c289bc6bf42446bac
bingoYB <binhdu@foxmail.com>
2025-03-09 21:04:25 +0800

    fix version check

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
+  eslint: {
+    ignoreDuringBuilds: true
+  },
   /* config options here */
   typescript: {
     ignoreBuildErrors: true,
